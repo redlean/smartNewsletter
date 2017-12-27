a898d193f61c8c6cdcf659e260db0341668a25b8
hazem.belkhiria <hazem.belkhiria@redlean.io>
2017-12-27 17:14:34 +0100

    init

---
+++ src/main/java/com/redlean/news/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.redlean.news.config.audit;
---
+++ src/main/java/com/redlean/news/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.redlean.news.security;
+
+import com.redlean.news.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        return SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT);
+    }
+}
